1d71ae10c94913790f74bf9d886b5e4feb430094
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-26 04:05:23 -0500

    Added dotbot, to replace custom script

--- symlink-dotfiles.sh
+++
@@ -1,9 +0,0 @@
-cd ~/dotfiles
-git pull
-
-# symlink our script
-rm ~/symlink-dotfiles.sh
-ln -sf ~/dotfiles/symlink-dotfiles.sh ~/symlink-dotfiles.sh
-
-# add symlinks for all our dotfiles here
-ln -sf ~/dotfiles/.spacemacs ~/.spacemacs
