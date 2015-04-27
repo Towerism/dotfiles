60ae127b9905f509ff7f15c377c0fa50ffc2638b
Martin Fracker <martin.frackerjr@gmail.com>
2015-04-26 23:38:41 -0500

    Fixed bullshit mistakes

--- emacs.d/private/martin-c++/packages.el
+++ emacs.d/private/martin-c++/packages.el
@@ -12,12 +12,28 @@
 (defvar martin-c++-packages
   '(
     cpputils-cmake
-                            auto-complete-clang
-                            ;;cmake-font-lock
-                            flycheck
-                            cmake-ide
-                            )
+    auto-complete-clang
+    cmake-font-lock
+    cmake-ide
+    )
   )
+
+(defun martin-c++/init-cmake-ide ()
+  (cmake-ide-setup)
+  )
+
+(defun my-ac-config ()
+  (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
+  (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
+  ;; (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
+  (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
+  (add-hook 'css-mode-hook 'ac-css-mode-setup)
+  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
+  (global-auto-complete-mode t))
+(defun my-ac-cc-mode-setup ()
+  (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))
+(add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
+
 
 (defun martin-c++/init-auto-complete-clang ()
   (add-to-list 'load-path "~/AC")
@@ -28,39 +44,9 @@
 
   (setq ac-auto-start nil)
   (setq ac-quick-help-delay 0.5)
+  (define-key ac-mode-map  [(control tab)] 'auto-complete)
+  (my-ac-config)
   )
-
-(defun martin-c++/init-cmake-ide ()
-  ;; (require 'rtags)
-  (cmake-ide-setup)
-  )
-
-;;(defun my-ac-config ()
-;;  (setq-default ac-sources '(ac-source-abbrev ac-source-dictionary ac-source-words-in-same-mode-buffers))
-;;  (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
-;;  ;; (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
-;;  (add-hook 'ruby-mode-hook 'ac-ruby-mode-setup)
-;;  (add-hook 'css-mode-hook 'ac-css-mode-setup)
-;;  (add-hook 'auto-complete-mode-hook 'ac-common-setup)
-;;  (global-auto-complete-mode t))
-;;(defun my-ac-cc-mode-setup ()
-;;  (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))
-;;(add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
-;;
-;;(defun martin-c++/init-auto-complete-clang ()
-;;  (add-to-list 'load-path (concat myoptdir "AC"))
-;;  (use-package auto-complete-config)
-;;  (add-to-list 'ac-dictionary-directories (concat myoptdir "AC/ac-dict"))
-;;
-;;  (use-package'auto-complete-clang)
-;;
-;;  (setq ac-auto-start nil)
-;;  (setq ac-quick-help-delay 0.5)
-;;  ;; (ac-set-trigger-key "TAB")
-;;  ;; (define-key ac-mode-map  [(control tab)] 'auto-complete)
-;;  (define-key ac-mode-map  [(control tab)] 'auto-complete)
-;;  (my-ac-config)
-;;  )
 
 (defun martin-c++/init-cpputils-cmake ()
   (use-package cpputils-cmake
