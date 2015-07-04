f860f620dda7af962d20dbae9fb152b0627bdb51
Martin Fracker <martin.frackerjr@gmail.com>
2015-07-04 16:17:36 -0500

    Changed copyright

--- emacs.d/private/martin-c++/packages.el
+++ emacs.d/private/martin-c++/packages.el
@@ -1,7 +1,7 @@
 ;;; packages.el --- martin-c++ Layer packages File for Spacemacs
 ;;
 ;; Copyright (c) 2012-2014 Sylvain Benner
-;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;; Copyright (c) 2014-2015 Martin Fracker & Contributors
 ;;
 ;; Author: Sylvain Benner <sylvain.benner@gmail.com>
 ;; URL: https://github.com/syl20bnr/spacemacs
