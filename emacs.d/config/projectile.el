d5092cba13e51d5ae2d2358e41cd296738e5df4a
Martin Fracker <martin.frackerjr@gmail.com>
2016-01-07 03:34:24 -0600

    Add hotkey to invalidate cache

--- emacs.d/config/projectile.el
+++ emacs.d/config/projectile.el
@@ -6,6 +6,7 @@
 (evil-leader/set-key
   "pp" 'helm-projectile-switch-project
   "pf" 'helm-projectile-find-file
+  "pi" 'projectile-invalidate-cache
   "pc" 'projectile-compile-project
   "mgs" 'projectile-find-other-file
 )
