1a7f6078a5b83934c7adc4b4702ce6eb26e4596e
Martin Fracker <martin.frackerjr@gmail.com>
2016-01-07 02:37:22 -0600

    Get rid of superfluous nonsense

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,20 +6,12 @@
         package-archives)
 (package-initialize)
 
-(if (not (package-installed-p 'use-package))
-    (progn
-      (package-refresh-contents)
-      (package-install 'use-package)))
-
-(require 'use-package)
-
-(use-package cask)
 (require 'cask)
 (cask-initialize)
 
 (require 'pallet)
 (pallet-mode t)
-(use-package pallet)
+
 
 (load "~/.emacs.d/load-directory")
 (load-directory "~/.emacs.d/config")
