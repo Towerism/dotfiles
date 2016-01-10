075443ecc7e94a4af03872125a0e6d84caec0ac2
Martin Fracker <martin.frackerjr@gmail.com>
2016-01-09 23:53:56 -0600

    Add hotkey to fill paragraph in org mode

--- emacs.d/inits/lang/init-org.el
+++ emacs.d/inits/lang/init-org.el
@@ -1,6 +1,7 @@
 (require 'org-autolist)
 (require 'org-bullets)
 (require 'evil-org)
+(require 'init-evil-leader)
 
 (add-hook 'org-mode-hook
           'org-autolist-mode)
@@ -14,6 +15,17 @@
 (add-hook 'org-mode-hook
           'org-indent-mode)
 
+(defun org-auto-fill ()
+  (auto-fill-mode)
+  (set-fill-column 80))
+
+(add-hook 'org-mode-hook
+          'org-auto-fill)
+
 (setq org-startup-indented t)
+
+(evil-leader/set-key-for-mode 'org-mode
+  "mf" 'fill-paragraph
+  )
 
 (el-init-provide)
