14a6b82a68b4dba57270c8017e04233e030a5f7f
Martin Fracker <martin.frackerjr@gmail.com>
2016-01-09 22:45:22 -0600

    Add hooks for autolist, bullets, and indent

--- emacs.d/inits/lang/init-org.el
+++ emacs.d/inits/lang/init-org.el
@@ -1,4 +1,12 @@
+(require 'org-autolist)
+(require 'org-bullets)
 (require 'evil-org)
+
+(add-hook 'org-mode-hook
+          'org-autolist-mode)
+
+(add-hook 'org-mode-hook
+          'org-bullets-mode)
 
 (add-hook 'org-mode-hook
           'org-indent-mode)
