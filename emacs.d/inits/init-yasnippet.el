a87928b0b1c5c6a9f8d719e2ee0f3d57c9103bd3
Martin Fracker <martin.frackerjr@gmail.com>
2016-01-10 19:10:42 -0600

    Move yasnippet initialization to init-yasnippet

---
+++ emacs.d/inits/init-yasnippet.el
@@ -0,0 +1,4 @@
+(require 'yasnippet)
+
+(add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets")
+(yas-reload-all)
--- emacs.d/inits/lang/init-prog.el
+++ emacs.d/inits/lang/init-prog.el
@@ -1,11 +1,8 @@
-(require 'yasnippet)
 (require 'init-smartparens)
 
 (defun my-prog-mode-hook ()
   (smartparens-mode)
   (yas-minor-mode))
 
-(add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets")
-(yas-reload-all)
 (add-hook 'prog-mode-hook
           'my-prog-mode-hook)
