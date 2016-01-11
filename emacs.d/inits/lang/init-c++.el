7b01dcb401e5d9ea2ef6a8512bbd6481d7cd6b8a
Martin Fracker <martin.frackerjr@gmail.com>
2016-01-11 16:41:23 -0600

    Change [0] to 0 for readability

--- emacs.d/inits/lang/init-c++.el
+++ emacs.d/inits/lang/init-c++.el
@@ -2,7 +2,7 @@
 
 (defconst my-c++-style
   '("cc-mode"
-    (c-offsets-alist . ((innamespace . [0])))))
+    (c-offsets-alist . ((innamespace . 0)))))
 (c-add-style "my-c++" my-c++-style)
 
 (defun use-my-c++-style ()
