a9de65b4cee1683a1e956b8e1bad57b396b4c84e
Martin Fracker <martin.frackerjr@gmail.com>
2016-01-12 21:45:50 -0600

    Separate out package.el initialization

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,11 +1,3 @@
-;; install enable marmalade and melpa package repos
-(require 'package)
-  (push '("marmalade" . "http://marmalade-repo.org/packages/")
-        package-archives )
-  (push '("melpa" . "http://melpa.milkbox.net/packages/")
-        package-archives)
-(package-initialize)
-
 (require 'cask)
 (cask-initialize)
 
---
+++ emacs.d/inits/init-package.el
@@ -0,0 +1,8 @@
+(require 'package)
+  (push '("marmalade" . "http://marmalade-repo.org/packages/")
+        package-archives )
+  (push '("melpa" . "http://melpa.milkbox.net/packages/")
+        package-archives)
+(package-initialize)
+
+(el-init-provide)
