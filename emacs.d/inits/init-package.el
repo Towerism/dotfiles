7daa06b6e0ecdb9e995cbc4cd70f09ee2df9c068
Martin Fracker <martin.frackerjr@gmail.com>
2016-01-13 02:17:52 -0600

    Remove marmalade as a source

--- emacs.d/inits/init-package.el
+++ emacs.d/inits/init-package.el
@@ -1,6 +1,4 @@
 (require 'package)
-  (push '("marmalade" . "http://marmalade-repo.org/packages/")
-        package-archives )
   (push '("melpa" . "http://melpa.milkbox.net/packages/")
         package-archives)
 (package-initialize)
