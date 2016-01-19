36b6c6291916ff69733f25bafcb273d5b6fca803
Martin Fracker <martin.frackerjr@gmail.com>
2016-01-19 11:52:21 -0600

    Add agenda file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -10,3 +10,15 @@
 
 (let ((custom-override-file "~/.emacs.d/custom.el"))
   (when (file-exists-p custom-override-file) (load-file custom-override-file)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/notes/geog/203/todo.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
