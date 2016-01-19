7ca3dfeaac786528f942392677e1895176354444
Martin Fracker <martin.frackerjr@gmail.com>
2016-01-19 13:58:04 -0600

    Revert "Initialize calendar when org mode starts"
    This reverts commit f78d382a29bc68307d96aa19910f74092b526a04.

--- emacs.d/inits/lang/init-org.el
+++ emacs.d/inits/lang/init-org.el
@@ -2,15 +2,13 @@
 (require 'org-bullets)
 (require 'evil-org)
 (require 'init-evil-leader)
-(require 'init-evil)
 
 (defun my-org-mode-hook ()
   (org-autolist-mode)
   (org-bullets-mode)
   (evil-org-mode)
   (org-indent-mode)
-  (org-auto-fill)
-  (org-init-calendar))
+  (org-auto-fill))
 
 (add-hook 'org-mode-hook
           'my-org-mode-hook)
@@ -18,10 +16,6 @@
 (defun org-auto-fill ()
   (auto-fill-mode)
   (set-fill-column 80))
-
-(defun org-init-calendar ()
-  (org-goto-calendar)
-  (delete-window))
 
 (setq org-startup-indented t
       org-log-done 'time)
