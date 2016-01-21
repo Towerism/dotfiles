2117bfedcac4b0c879a14d16d54d4ddc9fdc5455
Martin Fracker <martin.frackerjr@gmail.com>
2016-01-21 01:21:01 -0600

    Use lazy loading for init files

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,7 +6,8 @@
 
 (require 'el-init)
 (el-init-load "~/.emacs.d/inits"
-              :subdirectories '("." "evil" "lang" "version-control" "visual"))
+              :subdirectories '("." "evil" "lang" "version-control" "visual")
+              :wrappers '(el-init-require/lazy))
 
 (let ((custom-override-file "~/.emacs.d/custom.el"))
   (when (file-exists-p custom-override-file) (load-file custom-override-file)))
