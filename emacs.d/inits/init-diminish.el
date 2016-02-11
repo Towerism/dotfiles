f86f84d73bc25652e6640a39c642acabfb84d785
Martin Fracker <martin.frackerjr@gmail.com>
2016-02-11 15:49:36 -0600

    Diminish several modes

--- emacs.d/inits/init-diminish.el
+++ emacs.d/inits/init-diminish.el
@@ -1,3 +1,11 @@
 (require 'diminish)
 
+(diminish 'projectile-mode (format " P[%s]" (projectile-project-name)))
+(diminish 'auto-revert-mode)
+(diminish 'evil-escape-mode)
+(diminish 'smartparens-mode)
+(diminish 'undo-tree-mode)
+(diminish 'yas-minor-mode)
+(diminish 'company-mode)
+
 (el-init-provide)
