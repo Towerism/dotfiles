32674d40931c4e115b7fefd2ecc6577dae14350d
Martin Fracker <martin.frackerjr@gmail.com>
2016-02-16 14:50:35 -0600

    Don't diminish projectile-mode

--- emacs.d/inits/init-diminish.el
+++ emacs.d/inits/init-diminish.el
@@ -1,6 +1,5 @@
 (require 'diminish)
 
-(diminish 'projectile-mode (format " P[%s]" (projectile-project-name)))
 (diminish 'auto-revert-mode)
 (diminish 'evil-escape-mode)
 (diminish 'smartparens-mode)
