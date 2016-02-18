bb5a65859334486091f859a30b28cab57fe320b1
Martin Fracker <martin.frackerjr@gmail.com>
2016-02-18 14:25:21 -0600

    Add back projectile diminish and require inits

--- emacs.d/inits/init-diminish.el
+++ emacs.d/inits/init-diminish.el
@@ -1,5 +1,11 @@
 (require 'diminish)
+(require 'init-evil)
+(require 'init-smartparens)
+(require 'init-yasnippet)
+(require 'init-projectile)
+(require 'init-company)
 
+(diminish 'projectile-mode (format "P[%s]" (projectile-project-name)))
 (diminish 'auto-revert-mode)
 (diminish 'evil-escape-mode)
 (diminish 'smartparens-mode)
