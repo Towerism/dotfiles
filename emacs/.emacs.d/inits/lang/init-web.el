ff5aba26b2fa5092ddcdc025724ef4a6acedc6da
Martin Fracker <martin.frackerjr@gmail.com>
2016-04-30 13:03:38 -0500

    Add web mode init

---
+++ emacs/.emacs.d/inits/lang/init-web.el
@@ -0,0 +1,11 @@
+(require 'web-mode)
+
+(defun my-web-mode-hook ()
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  )
+
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+
+(el-init-provide)
