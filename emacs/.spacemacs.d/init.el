2ad7d8d385860b907fc213fbc83022fa7512c63c
Martin Fracker <martin.frackerjr@gmail.com>
2016-09-08 15:34:35 -0500

    Enable global linum mode

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -253,6 +253,7 @@ This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
   (add-hook 'org-mode-hook 'auto-fill-mode)
+  (global-linum-mode)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
