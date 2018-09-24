6fc6986753407f90f659d6c61b2098f9dda6a1a8
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-09-24 18:57:53 -0500

    Fix warnings

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -72,7 +72,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(monokai-theme)
+   dotspacemacs-additional-packages '()
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages and/or extensions that will not be install and loaded.
@@ -137,10 +137,9 @@ values."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
-                         monokai
-                         solarized-dark
                          spacemacs-dark
                          spacemacs-light
+                         solarized-dark
                          solarized-light
                          leuven
                          zenburn)
@@ -148,8 +147,8 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
    ;; size to make separators look not too crappy.
-   dotspacemacs-default-font '("Source Code Pro Regular"
-                               :size 13
+   dotspacemacs-default-font '("Source Code Pro"
+                               :size 16
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
