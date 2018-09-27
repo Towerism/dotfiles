1e02f5159cc88c83d5dee92f507e9aa437aa1f99
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-09-27 09:41:05 -0500

    Load polybar for each monitor

--- i3/.config/polybar/launch.sh
+++ i3/.config/polybar/launch.sh
@@ -6,8 +6,19 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+function loadbars {
+    echo "loading polybar on monitor $1"
+    MONITOR=$1 polybar --reload example &
+}
+
 # Launch bar1 and bar2
-polybar example &
+if type "xrandr"; then
+    for m in $(polybar --list-monitors | cut -d":" -f1); do
+        loadbars $m
+    done
+else
+    loadbars
+fi
 
 echo "Bars launched..."
 
