52603f30b0e6d2c96eab7b64c2d7be3ade4c054a
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-09-30 16:58:29 -0500

    Calculate background-alt color from background color

--- i3/.config/polybar/launch.sh
+++ i3/.config/polybar/launch.sh
@@ -3,11 +3,14 @@
 # Terminate already running bar instances
 killall -q polybar
 
+export LIGHTER_BACKGROUND=$(color-blend whiten --factor 0.1 $BACKGROUND)
+
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 function loadbars {
     echo "loading polybar on monitor $1"
+    lighter_background=~/
     MONITOR=$1 polybar --reload example &
 }
 
