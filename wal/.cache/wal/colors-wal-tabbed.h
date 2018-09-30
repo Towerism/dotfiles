d97f56359119d3f8e3e35bf8aa80292f483ea38b
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-09-29 20:27:06 -0500

    Remove .cache

--- wal/.cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#0B1A2F";
-static const char* selfgcolor   = "#cacad1";
-static const char* normbgcolor  = "#585886";
-static const char* normfgcolor  = "#cacad1";
-static const char* urgbgcolor   = "#595B75";
-static const char* urgfgcolor   = "#cacad1";
