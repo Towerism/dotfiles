487cb2ecedf1cd723cf9a332785e13e8e2337b4d
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-10-06 14:09:33 -0500

    Reformat config with two space indent

--- oni/.config/oni/config.tsx
+++ oni/.config/oni/config.tsx
@@ -2,49 +2,49 @@ import * as React from "react"
 import * as Oni from "oni-api"
 
 export const activate = (oni: Oni.Plugin.Api) => {
-    console.log("config activated")
+  console.log("config activated")
 
-    // Input
-    //
-    // Add input bindings here:
-    //
-    oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"))
+  // Input
+  //
+  // Add input bindings here:
+  //
+  oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"))
 
-    //
-    // Or remove the default bindings here by uncommenting the below line:
-    //
-    // oni.input.unbind("<c-p>")
+  //
+  // Or remove the default bindings here by uncommenting the below line:
+  //
+  // oni.input.unbind("<c-p>")
 
 }
 
 export const deactivate = (oni: Oni.Plugin.Api) => {
-    console.log("config deactivated")
+  console.log("config deactivated")
 }
 
 export const configuration = {
-    //add custom config here, such as
+  //add custom config here, such as
 
-    "ui.colorscheme": "monokai",
-    "oni.bookmarks": ["~/source/repos"],
-    "oni.loadInitVim": true,
-    "oni.useDefaultConfig": false,
+  "ui.colorscheme": "monokai",
+  "oni.bookmarks": ["~/source/repos"],
+  "oni.loadInitVim": true,
+  "oni.useDefaultConfig": false,
 
-    // UI customizations
-    "oni.hideMenu": true,
-    "ui.animations.enabled": true,
-    "ui.fontSmoothing": "auto",
-    "editor.fontSize": "16px",
-    "editor.fontWeight": "normal",
-    "editor.fontFamily": "Source Code Pro",
-    "sidebar.enabled": false
+  // UI customizations
+  "oni.hideMenu": true,
+  "ui.animations.enabled": true,
+  "ui.fontSmoothing": "auto",
+  "editor.fontSize": "16px",
+  "editor.fontWeight": "normal",
+  "editor.fontFamily": "Source Code Pro",
+  "sidebar.enabled": false
 
-    // Language servers
-    "language.bash.languageServer.command": "bash-language-server",
-    "language.bash.languageServer.arguments": ["start"],
+  // Language servers
+  "language.bash.languageServer.command": "bash-language-server",
+  "language.bash.languageServer.arguments": ["start"],
 
-    "language.dockerfile.languageServer.command": "docker-langserver",
-    "language.dockerfile.languageServer.arguments": ["--stdio"],
+  "language.dockerfile.languageServer.command": "docker-langserver",
+  "language.dockerfile.languageServer.arguments": ["--stdio"],
 
-    "language.vue.languageServer.command": "vls",
-    "language.vue.languageServer.arguments": ["--stdio"]
+  "language.vue.languageServer.command": "vls",
+  "language.vue.languageServer.arguments": ["--stdio"]
 }
