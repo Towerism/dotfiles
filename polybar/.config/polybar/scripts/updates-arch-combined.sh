2fa49a8111b850cdd45ca95db8775c314912ee27
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-10-18 14:20:48 -0500

    Fix the yay AUR update check

--- polybar/.config/polybar/scripts/updates-arch-combined.sh
+++ polybar/.config/polybar/scripts/updates-arch-combined.sh
@@ -5,8 +5,8 @@ set -o pipefail
 if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
     updates_arch=0
 fi
-
-if ! updates_aur=$(yay -Qum  | wc -l) || [ "$?" -ne 0 ]; then
+updates_aur=$(yay -Qum | wc -l) 
+if [ "$?" -ne 0 ]; then
 # if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
 # if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
     updates_aur=0
